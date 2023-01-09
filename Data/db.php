c7f3ad55c58d8caccfb5b67f3a9bf6169fb79381
32Fire32 <nicola.gianfelici@hotmail.it>
2023-01-09 17:49:05 +0100

    bonus 2 stampa a schermo layout con 10 film e le loro proprietà

---
+++ Data/db.php
@@ -0,0 +1,31 @@
+<?php
+
+$indiana_jones = new Movie('Indiana Jones', ['Avventura', 'Azione'], 'Steven Spielberg', '1981');
+$indiana_jones->setTotalCashed(9850000);
+
+$predator2 = new Movie('Predator 2', ['Horror/SciFi','Azione'], 'Stephen Hopkins', '1990');
+$predator2->setTotalCashed(850000);
+
+$starship_troopers = new Movie('Starship Troopers', ['Horror/SciFi','Azione'], 'Paul Verhoeven', '1997');
+$starship_troopers->setTotalCashed(450000);
+
+$fantozzi = new Movie('Fantozzi', ['Commedia'], 'Luciano Salce', '1975');
+$fantozzi->setTotalCashed(5850000);
+
+$treehundred = new Movie('300', ['Storico','Azione'], 'Zack Snyder', '2007');
+$treehundred->setTotalCashed(1850000);
+
+$beowulf = new Movie('La Leggenda di Beowulf', ['3D','Azione'], 'Robert Zemeckis', '2007');
+$beowulf->setTotalCashed(350000);
+
+$titanic = new Movie('Titanic', ['Storico','Drammatico'], 'James Cameron', '1998');
+$titanic->setTotalCashed(9850000);
+
+$lord_rings = new Movie('Il Signore degli Anelli', ['Avventura','Azione', 'Epico'], 'Peter Jackson', '2001');
+$lord_rings->setTotalCashed(7850000);
+
+$pierino = new Movie('Pierino Contro Tutti', ['Comico','Demenziale'], 'Marino Girolami', '1981');
+$pierino->setTotalCashed(150000);
+
+$allenatore = new Movie('L\'Allenatore nel Pallone', ['Comico','Demenziale'], 'Sergio Martino', '1984');
+$allenatore->setTotalCashed(350000);
--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 require_once __DIR__ . '/Classes/Movie.php';
-
-$indiana_genre = ['Avventura', 'Azione'];
-$indiana_jones = new Movie('Indiana Jones', $indiana_genre, 'Steven Spielberg', '1981');
-$predator2_genre = ['Horror/SciFi','Azione'];
-$predator2 = new Movie('Predator 2', $predator2_genre, 'Stephen Hopkins', '1990');
-
+require_once __DIR__ . '/Data/db.php';
 ?>
 
 <!DOCTYPE html>
@@ -14,19 +9,92 @@ $predator2 = new Movie('Predator 2', $predator2_genre, 'Stephen Hopkins', '1990'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Movies</title>
 </head>
 <body>
-    <h1>Titolo: <?php echo $indiana_jones->getTitle(); ?></h1>
-    <h3>Genere: <?php echo $indiana_jones->getGenre()[0];?> <?php echo $indiana_jones->getGenre()[1]; ?></h3>
-    <h3>Regista: <?php echo $indiana_jones->getMovieDirector(); ?></h3>
-    <h3>Anno: <?php echo $indiana_jones->getYear(); ?></h3>
-    <h3>Giudizio finale: <?php $indiana_jones->setTotalCashed(9250523); echo $indiana_jones->getTotalCashed(); ?></h3>
-
-    <h1>Titolo: <?php echo $predator2->getTitle(); ?></h1>
-    <h3>Genere: <?php echo $predator2->getGenre()[0];?> <?php echo $predator2->getGenre()[1]; ?></h3>
-    <h3>Regista: <?php echo $predator2->getMovieDirector(); ?></h3>
-    <h3>Anno: <?php echo $predator2->getYear(); ?></h3>
-    <h3>Giudizio finale: <?php $predator2->setTotalCashed(899000); echo $predator2->getTotalCashed(); ?></h3>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Titolo</th>
+      <th scope="col">Genere</th>
+      <th scope="col">Regista</th>
+      <th scope="col">Anno</th>
+      <th scope="col">Giudizio Finale</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"><?php echo $indiana_jones->getTitle()?></th>
+      <td><?php echo $indiana_jones->getGenre()[0]?> <?php echo $indiana_jones->getGenre()[1]?></td>
+      <td><?php echo $indiana_jones->getMovieDirector()?></td>
+      <td><?php echo $indiana_jones->getYear()?></td>
+      <td><?php echo $indiana_jones->getTotalCashed()?></td>
+    </tr>
+    <tr>
+      <th scope="row"><?php echo $predator2->getTitle()?></th>
+      <td><?php echo $predator2->getGenre()[0]?> <?php echo $predator2->getGenre()[1]?></td>
+      <td><?php echo $predator2->getMovieDirector()?></td>
+      <td><?php echo $predator2->getYear()?></td>
+      <td><?php echo $predator2->getTotalCashed()?></td>
+    </tr>
+    <tr>
+    <th scope="row"><?php echo $starship_troopers->getTitle()?></th>
+      <td><?php echo $starship_troopers->getGenre()[0]?> <?php echo $starship_troopers->getGenre()[1]?></td>
+      <td><?php echo $starship_troopers->getMovieDirector()?></td>
+      <td><?php echo $starship_troopers->getYear()?></td>
+      <td><?php echo $starship_troopers->getTotalCashed()?></td>
+    </tr>
+    <tr>
+    <th scope="row"><?php echo $fantozzi->getTitle()?></th>
+      <td><?php echo $fantozzi->getGenre()[0]?> <?php echo $fantozzi->getGenre()[1]?></td>
+      <td><?php echo $fantozzi->getMovieDirector()?></td>
+      <td><?php echo $fantozzi->getYear()?></td>
+      <td><?php echo $fantozzi->getTotalCashed()?></td>
+    </tr>
+    <tr>
+    <th scope="row"><?php echo $treehundred->getTitle()?></th>
+      <td><?php echo $treehundred->getGenre()[0]?> <?php echo $treehundred->getGenre()[1]?></td>
+      <td><?php echo $treehundred->getMovieDirector()?></td>
+      <td><?php echo $treehundred->getYear()?></td>
+      <td><?php echo $treehundred->getTotalCashed()?></td>
+    </tr>
+    <tr>
+    <th scope="row"><?php echo $beowulf->getTitle()?></th>
+      <td><?php echo $beowulf->getGenre()[0]?> <?php echo $beowulf->getGenre()[1]?></td>
+      <td><?php echo $beowulf->getMovieDirector()?></td>
+      <td><?php echo $beowulf->getYear()?></td>
+      <td><?php echo $beowulf->getTotalCashed()?></td>
+    </tr>
+    <tr>
+    <th scope="row"><?php echo $titanic->getTitle()?></th>
+      <td><?php echo $titanic->getGenre()[0]?> <?php echo $titanic->getGenre()[1]?></td>
+      <td><?php echo $titanic->getMovieDirector()?></td>
+      <td><?php echo $titanic->getYear()?></td>
+      <td><?php echo $titanic->getTotalCashed()?></td>
+    </tr>
+    <tr>
+    <th scope="row"><?php echo $lord_rings->getTitle()?></th>
+      <td><?php echo $lord_rings->getGenre()[0]?> <?php echo $lord_rings->getGenre()[1]?></td>
+      <td><?php echo $lord_rings->getMovieDirector()?></td>
+      <td><?php echo $lord_rings->getYear()?></td>
+      <td><?php echo $lord_rings->getTotalCashed()?></td>
+    </tr>
+    <tr>
+    <th scope="row"><?php echo $pierino->getTitle()?></th>
+      <td><?php echo $pierino->getGenre()[0]?> <?php echo $pierino->getGenre()[1]?></td>
+      <td><?php echo $pierino->getMovieDirector()?></td>
+      <td><?php echo $pierino->getYear()?></td>
+      <td><?php echo $pierino->getTotalCashed()?></td>
+    </tr>
+    <tr>
+    <th scope="row"><?php echo $allenatore->getTitle()?></th>
+      <td><?php echo $allenatore->getGenre()[0]?> <?php echo $allenatore->getGenre()[1]?></td>
+      <td><?php echo $allenatore->getMovieDirector()?></td>
+      <td><?php echo $allenatore->getYear()?></td>
+      <td><?php echo $allenatore->getTotalCashed()?></td>
+    </tr>
+  </tbody>
+</table>
 </body>
 </html>
